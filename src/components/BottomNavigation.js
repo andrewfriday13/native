8e9ccc92d9ebcbba18d5daf12b2dd1a17428d581
andrewfriday13 <110158654+andrewfriday13@users.noreply.github.com>
2023-07-24 12:00:14 +0200

    Update BottomNavigation.js

--- src/components/BottomNavigation.js
+++ src/components/BottomNavigation.js
@@ -1,38 +1,46 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import AntDesign from "react-native-vector-icons/AntDesign";
 import SimpleLineIcons from "react-native-vector-icons/SimpleLineIcons";
 import Ionicons from "react-native-vector-icons/Ionicons";
 import { LogoutComponent } from "./LogoutBtn";
+import { useRoute } from "@react-navigation/native";
 
 const Tab = createBottomTabNavigator();
 
 export const BottomNavigation = () => {
+  const route = useRoute();
+
   const Feed = () => {
     return (
-      <View style={{ width: 100, backgroundColor: "coral", flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <Text>Feed!</Text>
+      <View style={{ backgroundColor: "coral", flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <Text>Публікаціїї</Text>
       </View>
     );
   };
-  const Notifications = () => {
+  const createPost = () => {
+    // console.log(name);
+
     return (
-      <View>
-        <Text>werwer</Text>
+      <View style={{ backgroundColor: "blue", flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <Text>Створити</Text>
       </View>
     );
   };
   const Profile = () => {
+    console.log(route.name);
+
     return (
-      <View>
-        <Text>werwer</Text>
+      <View style={{ backgroundColor: "green", flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <Text>Профіль</Text>
       </View>
     );
   };
   return (
     <Tab.Navigator
-      initialRouteName="Feed"
+      initialRouteName="Публікаціїї"
       screenOptions={{
+        tabBarStyle: { height: 98 },
         headerStyle: { height: 108 },
         tabBarActiveTintColor: "#e91e63",
       }}
@@ -44,17 +52,30 @@ export const BottomNavigation = () => {
           tabBarLabel: null,
           tabBarShowLabel: false,
           headerRight: () => <LogoutComponent />,
-          tabBarIcon: ({ color, size }) => <SimpleLineIcons name="grid" color={color} size={size} />,
+          tabBarIcon: () => <SimpleLineIcons name="grid" color={"#212121"} size={20} />,
         }}
       />
       <Tab.Screen
         name="Створити публікацію"
-        component={Notifications}
+        component={createPost}
         options={{
           tabBarLabel: null,
           tabBarShowLabel: false,
-
-          tabBarIcon: ({ color, size }) => <Ionicons name="add-sharp" color={color} size={size} />,
+          tabBarIcon: () => (
+            <View
+              style={{
+                backgroundColor: "#FF6C00",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                borderRadius: 20,
+                width: 70,
+                height: 40,
+              }}
+            >
+              <Ionicons name="add-sharp" size={26} color={"white"} />
+            </View>
+          ),
         }}
       />
       <Tab.Screen
@@ -63,7 +84,7 @@ export const BottomNavigation = () => {
         options={{
           tabBarLabel: null,
           tabBarShowLabel: false,
-          tabBarIcon: () => <AntDesign name="user" color={"gray"} size={24} />,
+          tabBarIcon: () => <AntDesign name="user" color={"#212121"} size={20} />,
         }}
       />
     </Tab.Navigator>
